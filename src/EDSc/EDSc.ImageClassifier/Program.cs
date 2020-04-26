a174112a1200daed26ae45631342a7f7936b5ae8
Artem Lozhechkin <avlozhechkin@gmail.com>
2020-04-26 20:05:01 +0300

    Fix

---
+++ src/EDSc/EDSc.ImageClassifier/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using EDSc.Common.MessageBroker;
+using EDSc.Common.Model;
+using EDSc.Common.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.ML;
+
+namespace EDSc.ImageClassifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+            var configuration = builder.Build();
+            var rmqPublisher = new RmqPublisherBuilder()
+                .WithExchangeAutoCreation()
+                .UsingConfigExchangeAndRoutingKey(configuration.GetSection("RmqPublisher"))
+                .UsingCustomHost("localhost")
+                .Build();
+            var rmqConsumer = new RmqConsumerBuilder()
+                .UsingQueue("ImageClassifier")
+                .UsingCustomHost("localhost")
+                .Build();
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton(rmqPublisher)
+                .AddSingleton(rmqConsumer)
+                .AddPredictionEnginePool<InMemoryImageData, ImagePrediction>().FromFile("imageClassifier.zip")
+                .Services.BuildServiceProvider();
+            
+            var serv = new ImageClassificationService(
+                serviceProvider.GetService<IRmqConsumer>(),
+                serviceProvider.GetService<IRmqPublisher>(),
+                serviceProvider.GetService<PredictionEnginePool<InMemoryImageData, ImagePrediction>>());
+
+            serv.Start();
+        }
+    }
+}
